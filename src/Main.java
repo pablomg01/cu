e712213e56619d5a59c9a585f1b0557330c52118
pablo <pablomg01@hotmail.com>
2023-07-11 23:41:02 +0200

    nuuu4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Helloll nuevo   otro");
+        System.out.println("Helloll nuevo 55  otro");
     }
 }
